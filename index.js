4cf57668990f540d268c51feae1f372dbf499b56
arielschinocca <96122546+arielschinocca@users.noreply.github.com>
2024-05-13 20:21:10 -0300

    creando repo

---
+++ index.js
@@ -0,0 +1,111 @@
+
+
+
+
+// --------------------------------------------------------------------------
+//                          FUNCIONES
+//--------------------------------------------------------------------------
+//----- AUDIO 
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('La página se ha cargado completamente.');
+    var audio = document.getElementById('miAudio');
+    audio.play();
+});
+
+
+function botonColor(){
+    const boton = document.getElementById("play-pause-music");
+    boton.addEventListener("click", () => {
+
+    if( boton.style.backgroundColor == "green"){
+        boton.style.backgroundColor = "red" 
+
+    }else{
+        boton.style.backgroundColor = "green" 
+    }
+    // Genera colores aleatorios (puedes personalizar esto)
+    // const aleatorio1 = Math.floor(Math.random() * 256);
+    // const aleatorio2 = Math.floor(Math.random() * 256);
+    // const aleatorio3 = Math.floor(Math.random() * 256);
+
+    // Cambia el color de fondo del botón
+    // boton.style.backgroundColor = `rgb(${aleatorio1}, ${aleatorio2}, ${aleatorio3})`;
+    });
+}
+botonColor();
+//---------------------------------------------------------------------------
+//                           INICIO HTML
+//---------------------------------------------------------------------------
+window.onload("load", () => {
+
+    audioPlay();
+    audio.classList.add("play");
+    audio.src = "./audio/elvis.mp3";
+});
+
+//-----------------------------------------------------------------------------
+//                      BOTONES AUDIO
+// ----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
+//                      CARROUSEL
+// ----------------------------------------------------------------------------
+
+
+window.addEventListener("load", () => {
+    const glider = new Glider(document.querySelector('.list_carrousel'), {
+        slidesToScroll: 1,
+        slidesToShow: 2,
+        draggable: true,
+        dots: '.indice_carrousel',
+        arrows: {
+            prev: '.prev',
+            next: '.next'
+        },
+        responsive: [
+            {
+                // Pantallas mayores o iguales a 775px
+                breakpoint: 300,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    draggable: true,
+                }
+            }, {
+                // Pantallas mayores o iguales a 1024px
+                breakpoint: 800,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    draggable: true,
+                }
+            }
+        ]
+    });
+
+});
+
+//---------------------------------------------------------------------------
+// MODAL
+let botonModal = document.getElementById("botonModal");
+let modal = document.getElementById("contenedorModal");
+
+botonModal.addEventListener( "click", ()=>{
+
+
+    if( modal.style.display == "none"){
+        modal.style.display = "block";
+        return;
+    }
+
+    modal.style.display ="none";
+})
+
+let botonCerrarModal = document.getElementById("botonCerrarModal");
+let cerrarModal = document.getElementById("contenedorModal");
+
+botonCerrarModal.addEventListener( "click", ()=>{
+
+    cerrarModal.style.display ="none";
+})
+//-------------------------------------------------------------------------
